9c44fc81f33b9d93fc6d518673abc509f3af77d2
Wouter Vernaillen <wouter@vernaillen.com>
2019-03-17 10:32:01 +0100

    switched to vue-cli-service serve for local development

--- modules/triberay-vue-instagram-portlet/dev/index.es.js
+++ modules/triberay-vue-instagram-portlet/dev/index.es.js
@@ -1,10 +1,12 @@
-import Vue from 'vue/dist/vue.common';
-import App from '../src/main/resources/META-INF/resources/lib/Main.vue';
+import Vue from 'vue';
+import App from '../src/main/resources/META-INF/resources/lib/App.vue';
+import { store } from '../src/main/resources/META-INF/resources/lib/store/store';
+
+store.state.accessToken = '11085243931.be0cab3.7c1308b479ee488482f740ffc90f3cdd';
 
 new Vue({
 	el: `#devapp`,
-	render: h => h(App),
-	mounted() {
-		eventBus.$emit('setToken', '11085243931.be0cab3.7c1308b479ee488482f740ffc90f3cdd');
-	}
+	store,
+	template: '<App/>',
+	components: { App }
 });
