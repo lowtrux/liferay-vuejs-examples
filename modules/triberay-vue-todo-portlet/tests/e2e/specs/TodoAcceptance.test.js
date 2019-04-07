07f9e8c1de0424cabbb8ea2a8db25e46573c284a
Wouter Vernaillen <wouter@vernaillen.com>
2019-04-07 10:26:46 +0200

    fixed tests after removing Vuetify

--- modules/triberay-vue-todo-portlet/tests/e2e/specs/TodoAcceptance.test.js
+++ modules/triberay-vue-todo-portlet/tests/e2e/specs/TodoAcceptance.test.js
@@ -3,13 +3,12 @@ module.exports = {
         browser
             .url(process.env.VUE_DEV_SERVER_URL)
             .waitForElementVisible('#todoApp', 5000)
-            //.assert.elementPresent('.hello')
-            .assert.containsText('.v-toolbar__title', 'To Do App')
-            .assert.elementCount('.v-card', 1)
-            .assert.elementCount('.v-toolbar', 1)
+            .assert.containsText('a.navbar-brand', 'To Do App')
+            .assert.elementCount('nav.navbar', 1)
+            .assert.elementCount('ul.list-group', 1)
             .assert.elementCount('input', 1)
-            .assert.elementCount('.v-list__tile', 4)
-            .assert.containsText('.v-list__tile .v-list__tile__title', 'Learn Vue') // can only check the first <li> element
+            .assert.elementCount('li.list-group-item', 4)
+            .assert.containsText('li.list-group-item', 'Learn Vue') // can only check the first <li> element
             .end()
     }
 };
