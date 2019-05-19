0ad96a0610b635faddd8b237b0aa9744370d5b17
Wouter Vernaillen <wouter@vernaillen.com>
2019-05-19 13:50:12 +0200

    apio sample

---
+++ modules/triberay-vue-apio-portlet/src/main/java/com/triberay/vue/apio/portlet/constants/VueApioPortletKeys.java
@@ -0,0 +1,10 @@
+package com.triberay.vue.apio.portlet.constants;
+
+/**
+ * @author wouter
+ */
+public class VueApioPortletKeys {
+
+	public static final String TriberayVueApio = "com_triberay_vue_apio_portlet_VueApioPortlet";
+
+}
